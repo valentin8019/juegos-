01bcb7de5ea95c0380cc8a4b68914e3d3fedba5a
valentin8019 <107219820+valentin8019@users.noreply.github.com>
2022-06-11 19:13:35 -0300

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,11 @@ function nuevoEnemigo () {
         basic.pause(500)
         Enemigo.change(LedSpriteProperty.Y, 1)
     }
+    Enemigo = game.createSprite(randint(0, 4), 0)
+    for (let index = 0; index < 4; index++) {
+        basic.pause(500)
+        Enemigo.change(LedSpriteProperty.Y, 1)
+    }
 }
 input.onButtonPressed(Button.A, function () {
     cuadradito.move(-1)
@@ -15,3 +20,15 @@ let Enemigo: game.LedSprite = null
 let cuadradito: game.LedSprite = null
 cuadradito = game.createSprite(2, 4)
 nuevoEnemigo()
+basic.forever(function () {
+    if (Enemigo.isTouching(cuadradito)) {
+        basic.showString("PERDISTE")
+        basic.showLeds(`
+            # # . # #
+            # # . # #
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+    }
+})
